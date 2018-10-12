bc7a952e1798bfb1b14668c92c604bd0bf86a065
Andrew Waweru <andyweru@gmail.com>
2018-10-12 17:45:57 +0300

    added other models and registered them to admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Editor,Project,tags
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+
+admin.site.register(Editor)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(tags)
