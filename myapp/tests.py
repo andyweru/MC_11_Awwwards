6bdeb7c4d3d77edef7cffe3d8a144300b1d95401
Andrew Waweru <andyweru@gmail.com>
2018-10-15 16:12:37 +0300

    tests for project

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import *
+import datetime as dt
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    def setUp(self):
+        self.Pro= Editor(name = 'Wesh',)
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Pro,Editor))
+
+
+class ProjectsTestClass(TestCase):
+
+    def setUp(self):
+        self.Pro= Projects(name = 'InstaClone', description='Test project')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Pro,Projects))
+
+    def test_save_method(self):
+        self.Pro.save_project()
+        project = Projects.objects.all()
+        self.assertTrue(len(project) > 0)
+
+    def test_data(self):
+        self.assertTrue(self.Pro.name,"test")
+
+    def test_delete(self):
+        post = Projects.objects.filter(id=1)
+        post.delete()
+        posts = Projects.objects.all()
+        self.assertTrue(len(posts)==0)
